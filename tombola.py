16ceb79f49530c9fe139b0081d17d0b5a0b25b11
Soňa Falatková <sona.falatkova@seznam.cz>
2025-02-25 17:32:27 +0100

    první úkol tombola

---
+++ tombola.py
@@ -0,0 +1,11 @@
+"""V tombole bylo prodáno celkem 1000 lístků. Naším úkolem je vylosovat náhodně tři výherce. Napište program, který vygeneruje a vypíše tři čísla mezi 1 a 1000. Využijte funkci randint, nezapomeňte ale, že si ji musíte importovat z modulu random.
+
+Neřešte, že jedno číslo může být vygenerováno dvakrát."""
+
+from random import randint
+
+vyherci = []
+
+for i in range (3):
+    vyherci.append(randint(1, 1000))
+print(vyherci)
